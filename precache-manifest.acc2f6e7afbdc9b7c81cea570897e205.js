7139ce620ee4f14a0f7a9c66b75a8d807d06eb57
Vsevolod Maslak <vsevolodmaslak@gmail.com>
2021-08-31 16:26:30 +0300

    Updates

---
+++ precache-manifest.acc2f6e7afbdc9b7c81cea570897e205.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ead7190fdeff2cdc83ae8fb28a18436",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ce781a544626ffbfc529",
+    "url": "./static/css/main.8f037bf5.chunk.css"
+  },
+  {
+    "revision": "9cb276670027b79266c7",
+    "url": "./static/js/2.bafbf7af.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.bafbf7af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce781a544626ffbfc529",
+    "url": "./static/js/main.4b549f3e.chunk.js"
+  },
+  {
+    "revision": "c2acbe53827daed63a5f",
+    "url": "./static/js/runtime-main.1aec85e4.js"
+  }
+]);
